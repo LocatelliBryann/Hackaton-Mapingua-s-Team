f81b2b128b0b96894585b4daceb54979de29ebca
Viniobrabo <vinireisteur@gmail.com>
2022-06-24 21:18:40 -0300

    login-cadastro-semifeitas

---
+++ src/router/index.js
@@ -0,0 +1,47 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
+import telaInicial from "../views/register/telaInicial.vue";
+import cadastro from "../views/register/Cadastro.vue";
+import pagina1 from "../views/register/Pagina1.vue";
+import LoginVue from "../views/register/Login.vue";
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+
+        path: "/",
+        name: "Home",
+        component: Home,
+      
+  },
+  {
+    path: "/telaInicial",
+    name: "telaInicial",
+    component: telaInicial,
+  },
+  { 
+    path: "/login",
+    name: "login",
+    component: LoginVue
+  },
+  {
+    path: "/cadastro",
+    name: "cadastro",
+    component: cadastro,
+  },
+  {
+    path: "/pagina1",
+    name: "pagina1",
+    component: pagina1,
+  }
+];
+
+const router = new VueRouter({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes,
+});
+
+export default router;
---
+++ src/views/register/Login.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="main">
+    <div class="nav">
+      <div class="voltar">
+        <svg style="width: 24px; height: 24px" viewBox="0 0 24 24">
+          <path
+            fill="currentColor"
+            d="M20,11V13H8L13.5,18.5L12.08,19.92L4.16,12L12.08,4.08L13.5,5.5L8,11H20Z"
+          />
+        </svg>
+      </div>
+
+      <div class="titulo">
+        <h3>LOGIN</h3>
+      </div>
+
+      <div class="button-rtn"></div>
+    </div>
+
+    <div class="avatarElogin">
+      <div class="img-logo">
+        <v-avatar color="#D9D9D9" size="150"></v-avatar>
+      </div>
+
+      <div class="form">
+        <v-form ref="form">
+          <v-text-field label="Name" required></v-text-field>
+
+          <v-text-field label="E-mail" required></v-text-field>
+        </v-form>
+        <v-col>
+          <div class="my-5">
+            <v-btn color="#"> ENTRAR </v-btn>
+          </div>
+        </v-col>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+.main {
+  display: flex;
+  flex-direction: column;
+}
+.nav {
+  position: absolute;
+  width: 100%;
+  height: 5%;
+  border-bottom: 1px solid #000;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  top: 0;
+}
+
+.titulo {
+  padding-right: 2rem;
+}
+
+.form {
+  background-color: #f0fbe4;
+  color: #000000;
+  font-weight: 400;
+  height: 50px;
+  width: 23rem;
+  border-radius: 60px;
+  box-shadow: none;
+  border-style: groove;
+  margin-bottom: 150%;
+  animation: none;
+}
+
+.img-logo {
+  margin-bottom: 25%;
+}
+
+.v-btn.v-btn--is-elevated.v-btn--has-bg.theme--light.v-size--default {
+  background-color: #deefb8;
+  color: #000000;
+  font-weight: 400;
+  height: 50px;
+  width: 10rem;
+  border-radius: 60px;
+  box-shadow: none;
+  border-style: groove;
+}
+</style>
